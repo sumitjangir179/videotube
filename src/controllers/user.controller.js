d50bd3a9af0780021830d1d60eb9ff51a6f61531
sumit jangir <sumitjangir179@gmail.com>
2024-01-12 17:13:14 +0530

    user logout code modified

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -109,7 +109,8 @@ const loginUser = asyncHandler(async (req, res) => {
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
-    await User.findByIdAndUpdate(req.user._id, { $set: { refreshToken: undefined } }, { new: true })
+    await User.findByIdAndUpdate(req.user._id, { $unset: { refreshToken: 1 } }, { new: true })
+    // await User.findByIdAndUpdate(req.user._id, { $set: { refreshToken: undefined } }, { new: true })
     const options = { httpOnly: true, secure: true }
     res.status(200).clearCookie('accessToken', options).clearCookie("refreshToken", options).json(new ApiResponse(200, {}, 'User logout successfully'))
 });
